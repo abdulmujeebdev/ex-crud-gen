9e6268aae06ac2429aa0034d95b34e79707aced5
Abdul Mujeeb <abdul.mujeeb@bargoventures.com>
2021-10-20 17:19:43 +0500

    |

--- src/helper.js
+++ src/helper.js
@@ -1,8 +1,30 @@
+const fs = require("fs");
+
 export const replaceFromString = (text, replacementObject) => {
-  const replacementReg = new RegExp(Object.keys(replacementObject).join("|"), 'g');
+  const replacementReg = new RegExp(
+    Object.keys(replacementObject).join("|"),
+    "g"
+  );
   return text.replace(replacementReg, (e) => replacementObject[e]);
 };
 
-// file
-// read
-// write
+export const readFile = (filePath) => {
+  fs.readFile(filePath, (error, data) => {
+    if (data) {
+      return data.toString();
+    } else if (error) {
+      throw error;
+    }
+  });
+};
+
+export const writeFile = (filePath, content) => {
+  fs.mkdir(directory, { recursive: true }, (error) => {
+    if (error) throw error;
+    fs.writeFile(path, content, function (error) {
+      if (error) {
+        return null;
+      }
+    });
+  });
+};
